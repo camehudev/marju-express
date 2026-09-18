422b7309a9f3e5601af3c322c55f640ae45c4ec7
Carlos Humberto <camehudev@gmail.com>
2026-09-18 16:44:39 -0300

    atualização da API

--- main.py
+++ main.py
@@ -4,4 +4,15 @@ import uvicorn
 app = FastAPI()
 
 
+app = FastAPI(
+    title="Marju Express",
+    description="Sistema de controle de estoque para mercadorias (Shein, TikTok, etc.)",
+    version="1.0.0"
+)
+
+@app.get("/")
+def ler_raiz():
+    return {"mensagem": " Hellow Word! Bem-vindo ao Marju Express API! 🚀"}
+
+
 # uvicorn main:app --reload
