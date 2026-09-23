f24525a1ba58fdf3985f55cd10973b5bfd118600
Carlos Humberto <camehudev@gmail.com>
2026-09-23 16:03:18 -0300

    Ajuste de cors

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI(
     title="Marju Express",
     description="Sistema de controle de estoque para mercadorias (Shein, TikTok, etc.)",
     version="1.0.0"
+)
+
+# Configuração do CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Em produção, pode substituir por "http://localhost:4200" ou o domínio do seu frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],  # Permite todos os cabeçalhos
 )
 
 from app.routers.auth_router import auth_router
